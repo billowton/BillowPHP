b7fc0bcfd6772bca5f6981e0576d70f63871e0d7
billowton <billowton@foxmail.com>
2014-09-23 18:01:25 +0800

    修改默认控制器

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -5,10 +5,10 @@ use BillowPHP\Controller;
    class IndexController extends Controller{
 	    
 		public function index(){
-			 $hello = M('hello')->select();
-			 var_dump($hello);
-			 $this->assign('test','hello smarty');
-			 //$this->display();
+			// $hello = M('hello')->select();
+			 //var_dump($hello);
+			 $this->assign('hello','hello BillowPHP<br/>欢迎使用BillowPHP v0.1Beat版<br/>如有问题欢迎指正<br/>Author:billowton@foxmail.com');
+			 $this->display();
 		}
 		
 		public function __call($methodname,$argsarr){
