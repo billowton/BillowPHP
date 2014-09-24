cc164e99e26a4df14315f913c0048b6f14cb7b7d
billowton <billowton@foxmail.com>
2014-09-24 09:03:12 +0800

    修改默认配置

--- BillowPHP/Conf/Config.php
+++ BillowPHP/Conf/Config.php
@@ -1,12 +1,12 @@
 <?php
 return array(
  /* 数据库设置 */
-    'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  'localhost', // 服务器地址
-    'DB_NAME'               =>  'test',          // 数据库名
-    'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  'tanek',          // 密码
-    'DB_PORT'               =>  '3306',        // 端口
-    'DB_PREFIX'             =>  '',    // 数据库表前缀
+    'DB_TYPE'               =>  '',     // 数据库类型  暂时只支持mysql
+    'DB_HOST'               =>  '', // 服务器地址
+    'DB_NAME'               =>  '',          // 数据库名
+    'DB_USER'               =>  '',      // 用户名
+    'DB_PWD'                =>  '',          // 密码
+    'DB_PORT'               =>  '',        // 端口
+    //DB_PREFIX'             =>  '',    // 数据库表前缀   暂时不支持
 	);
 ?>
