78beeb9884332666a759a06d7fd7884f8e99d8c7
billowton <billowton@foxmail>
2014-09-30 16:41:45 +0800

    添加D() 方法

--- BillowPHP/Core/Function/function.php
+++ BillowPHP/Core/Function/function.php
@@ -10,3 +10,29 @@ use BillowPHP\Model;
 function M($model=null){
 	return new Model($model);
 }
+
+function D($model=null){
+
+   
+    if(!$model){
+    	die('error: D()方法参数不得为空');
+    }
+     global $module_path;
+	 global $moduleName;
+    //引入对应模块下的Model
+require_once($module_path.'/Model/'.$model.'Model.class.php');
+
+    //根据模型名 返回普通Model 还是ViewModel
+    if(strpos($model,'View')){
+    	$model_class_name_namespace = "$moduleName\\ViewModel\\$model"."Model";
+    }else{
+    	$model_class_name_namespace = "$moduleName\\Model\\$model"."Model";
+    }
+
+    
+
+    //返回对应的
+    return new $model_class_name_namespace();
+}
+
+
