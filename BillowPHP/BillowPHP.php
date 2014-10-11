c02cd37f1bc66d0175531745c5fd9bcc100c5832
billowton <billowton@foxmail>
2014-10-11 15:17:24 +0800

    修改初始界面

--- BillowPHP/BillowPHP.php
+++ BillowPHP/BillowPHP.php
@@ -21,20 +21,21 @@ define('CORE_PATH',ROOT_PATH.'BillowPHP/Core/');
 $query_string = $_SERVER['QUERY_STRING'];
 
 /**默认的模块名/控制器名/方法名*/
-//if(!defined('DEFAULT')){
+
+if(!defined(DEFAULT_MODULE)){
   $moduleName = 'Home';
-//}
-/*
-else{
-  $moduleName = DEFAULT;
-}*/
+}else{
+  $moduleName = DEFAULT_MODULE;
+}
+
+
 
 $controllerName = 'Index';
 $methodName = 'index';
 
 //如果没有目录创建初始结构
 //
-if('Home'=='Home'){
+if($moduleName=='Home'){
     if(!file_exists(APP_PATH)){
          mkdir(APP_PATH);  //创建应用目录
           if(!file_exists(APP_PATH.'Home/')){
@@ -52,7 +53,8 @@ use BillowPHP\Controller;
       
     public function index(){
        
-       $this->assign("hello","hello BillowPHP<br/>欢迎使用BillowPHP v0.1Release版<br/>如有问题欢迎指正<br/>Author:billowton@foxmail.com");
+       $this->assign("hello","<h1>Hello BillowPHP</h1>
+        <div class=\'body\'><p>欢迎使用BillowPHP v0.1Release版</p><p>如有问题欢迎指正</p><code>Author:billowton@foxmail.com</code></div>");
        $this->display();
     }
 
@@ -81,9 +83,48 @@ use BillowPHP\Controller;
   <meta charset=UTF-8>
   <meta name=Author content=billowton@foxmail.com>
   <title>BillowPHP欢迎页</title>
+   <style>
+         h1 {
+             color: #444;
+            background-color: transparent;
+            border-bottom: 1px solid #D0D0D0;
+             font-size: 19px;
+            font-weight: normal;
+            margin: 0 0 14px 0;
+            padding: 14px 15px 10px 15px;
+
+         }
+         .body{
+
+          margin: 0 15px 0 15px;
+          padding: 14px 15px 10px 15px;
+         }
+         p{
+          display: block;
+-webkit-margin-before: 1em;
+-webkit-margin-after: 1em;
+-webkit-margin-start: 0px;
+-webkit-margin-end: 0px;
+         }
+
+         code{
+          font-family: Consolas, Monaco, Courier New, Courier, monospace;
+font-size: 12px;
+background-color: #f9f9f9;
+border: 1px solid #D0D0D0;
+color: #002166;
+display: block;
+margin: 14px 0 14px 0;
+padding: 12px 10px 12px 10px;
+         }
+   </style>
  </head>
  <body>
-       <center><h1><font color=green>:){$hello}</font><h1></center>
+        <div style="margin: 30px;
+border: 1px solid #D0D0D0;
+-webkit-box-shadow: 0 0 8px #D0D0D0;">
+           {$hello}
+        </div>
  </body>
 </html>
 ';
