3474f9bfca58450bef4545f22942d013b4a862e5
billowton <billowton@foxmail>
2014-10-11 11:26:08 +0800

    入口文件版本号

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php 
+/**
+ * @version        v 0.1.7
+ * @author billowton <[billowton@foxmail.com]>
+**/
+//入口文件
 //引入BillowPHP核心文件
 
 //定义应用目录名字 默认为./App   (注，不得为空)
 define('APP','App');
 
-//默认模块 
-//define('DEFAULT','Home');
+//默认模块 module
+define('DEFAULT_MODULE','Home');
 require './BillowPHP/BillowPHP.php';
 
 ?>
